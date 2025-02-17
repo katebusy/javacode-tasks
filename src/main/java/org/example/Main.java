558c90d6ec4a0a96e92f752b3b17088d90b80c53
Ekaterina Busygina <0105kate54@gmail.com>
2025-02-17 17:28:51 +0300

    collection-1 updated main
    Signed-off-by: Ekaterina Busygina <0105kate54@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.collection.task1.Filter;
+import org.example.collection.task1.Utils;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Integer[] intArray = {0, 1, 2, 3, 4};
+        Filter intFilter = o -> (Integer) o + 1;
+
+        String[] strArray = {"0", "1", "2", "3", "4"};
+        Filter strFilter = o -> o + " applied";
+        Object[] newIntArray = Utils.filter(intArray, intFilter);
+        Object[] newStrArray = Utils.filter(strArray, strFilter);
+
+        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(newIntArray));
+
+        System.out.println(Arrays.toString(strArray));
+        System.out.println(Arrays.toString(newStrArray));
     }
 }
