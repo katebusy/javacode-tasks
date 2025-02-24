011e1ed8ef1290cffa3880820a89b1e638895a9f
Ekaterina Busygina <0105kate54@gmail.com>
2025-02-24 19:14:06 +0300

    stream-api-1
    Signed-off-by: Ekaterina Busygina <0105kate54@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+
+        Map<String, Double> map = orders.stream()
+                .collect(Collectors.groupingBy(
+                        Order::getProduct,
+                        Collectors.summingDouble(Order::getCost)))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                .limit(3)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        double sum = map.values().stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+        map.entrySet().forEach(System.out::println);
+        System.out.println("Total sum: " + sum);
     }
 }
